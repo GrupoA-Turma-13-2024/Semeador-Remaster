8afcb45abfd815afc94e88c45b3c669c9855cfc8
Villanuev-a <jhvmalavazi@gmail.com>
2024-10-31 09:59:28 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+// elementos para descer a tela
+
+document.getElementById('descer1').addEventListener('click', function(){
+document.getElementById('alvo').scrollIntoView({behavior:'smooth'
+});
+});
+
+document.getElementById('descer3').addEventListener('click', function(){
+    document.getElementById('alvo2').scrollIntoView({behavior:'smooth'
+});
+});
+
+document.getElementById('descer4').addEventListener('click', function(){
+    document.getElementById('alvo3').scrollIntoView({behavior:'smooth'
+});
+});
+
+document.getElementById('descer5').addEventListener('click', function(){
+    document.getElementById('alvo4').scrollIntoView({behavior:'smooth'
+});
+});
+
+
+document.getElementById('descer6').addEventListener('click', function(){
+    document.getElementById('alvo5').scrollIntoView({behavior:'smooth'
+});
+});
