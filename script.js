a642dd0c43e12123f87560c27ae094485cd468fe
Villanuev-a <jhvmalavazi@gmail.com>
2024-10-31 18:30:50 -0300

    commit

--- script.js
+++ script.js
@@ -33,14 +33,3 @@ document.querySelector('.texto_barra').addEventListener('click', function() {
 });
 
 // Fechar o dropdown se o usuário clicar fora dele
-window.onclick = function(event) {
-    if (!event.target.matches('.texto_barra')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        for (var i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
