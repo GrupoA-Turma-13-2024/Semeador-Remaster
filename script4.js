470a1b742750fc65f7d2538e5754af01e2a28306
Villanuev-a <jhvmalavazi@gmail.com>
2024-12-04 13:02:49 -0300

    termos na pag 4

--- script4.js
+++ script4.js
@@ -59,6 +59,18 @@ function lerElementos4() {
     }
   }
   
+
+  function botaotermo() {
+    var termoContainer = document.getElementById('termoContainer');
+    var escurecer = document.getElementById('escurecer');
+    if (termoContainer.style.display === 'block') {
+        termoContainer.style.display = 'none';
+        escurecer.style.display = 'none';
+    } else {
+        termoContainer.style.display = 'block';
+        escurecer.style.display = 'block';
+    }
+  }
   
   
   
